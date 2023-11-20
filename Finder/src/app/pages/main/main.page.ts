2ce56115d5866cdb9a6a110dc5d93d83832d9bb1
cokeastorgac <jor.astorga@duocuc.cl>
2023-11-20 17:17:28 -0300

    cre3acion del menu

--- Finder/src/app/pages/main/main.page.ts
+++ Finder/src/app/pages/main/main.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPage implements OnInit {
 
-  constructor() { }
+pages = [
+  {title: 'Inicio', url: 'home', icon: 'home-outline'},
+  {title: 'Perfil', url: 'prfile', icon: 'home-outline'}
+]
+
+router = inject(Router)
 
   ngOnInit() {
   }
