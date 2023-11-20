469f0ab037073ec7677c9183d4445d3c1115e335
Dottenk <martinrojas.marv@gmail.com>
2023-11-20 18:29:41 -0300

    xd

--- Finder/src/app/pages/main/home/home.page.ts
+++ Finder/src/app/pages/main/home/home.page.ts
@@ -112,7 +112,7 @@ async deleteProduct(product: Product) {
       message: 'Objeto eliminado correctamente',
       duration: 2800,
       color: 'success',
-      position: 'middle',
+      position: 'bottom',
       icon: 'checkmark-circle-outline',
     });
   } catch (error) {
@@ -122,7 +122,7 @@ async deleteProduct(product: Product) {
       message: error.message,
       duration: 2800,
       color: 'warning',
-      position: 'middle',
+      position: 'bottom',
       icon: 'alert-circle-outline',
     });
   } finally {
