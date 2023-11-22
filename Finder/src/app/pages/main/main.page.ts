4a4b3e029f305f16638c4f27be24f95701691ba8
cokeastorgac <jor.astorga@duocuc.cl>
2023-11-22 14:51:16 -0300

    color menu blanco

--- Finder/src/app/pages/main/main.page.ts
+++ Finder/src/app/pages/main/main.page.ts
@@ -20,7 +20,7 @@ export class MainPage implements OnInit {
  
 pages = [
   {title: 'Inicio', url: 'home', icon: 'home-outline'},
-  {title: 'Perfil', url: 'profile', icon: 'home-outline'}
+  {title: 'Perfil', url: 'profile', icon: 'person-outline'}
 ]
 
 router = inject(Router);
